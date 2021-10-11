5e6ef69bdd177d691a8ad9fa676be917659bfdba
ShapPo <slavko.ukrop@gmail.com>
2021-10-11 23:19:17 +0300

    now smart send will return message

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,7 @@
 import typing
 from discord_slash import ComponentContext
+from discord_slash.dpy_overrides import ComponentMessage
+from discord_slash.model import SlashMessage
 from discord.ext.commands import Context
 from discord import Member
 
@@ -7,7 +9,7 @@ from utils.setup import *
 from utils.actions import *
 
 
-async def smart_send(ctx: typing.Union[Context, ComponentContext], *args, do_not_edit=False, **kwargs):
+async def smart_send(ctx: typing.Union[Context, ComponentContext], *args, do_not_edit=False, **kwargs) -> typing.Optional[typing.Union[SlashMessage, ComponentMessage]]:
     """Send message or edit origin. Better version of something like:
 
     if isinstance(ctx, Context):
@@ -20,12 +22,12 @@ async def smart_send(ctx: typing.Union[Context, ComponentContext], *args, do_not
             if len(args) == 0:
                 if not 'delete_after' in kwargs:
                     kwargs['delete_after'] = 10
-                await ctx.send(*args, **kwargs)
-                return
+                return await ctx.send(*args, **kwargs)
             kwargs['content'] = args[0]
         await ctx.edit_origin(content=kwargs['content'])
+        return ctx.origin_message
     else:
-        await ctx.send(*args, **kwargs)
+        return await ctx.send(*args, **kwargs)
 
 
 def is_admin(member: Member) -> bool:
