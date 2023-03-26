ffbcfcfb88257d4f3021d5b873c6f2378a51e8d6
Shap <slavko.ukrop@gmail.com>
2023-03-26 14:56:28 +0300

    added an option to run multiple instances of a bot file "token.txt" must be renamed to "tokens.txt" with tokens

--- bot.py
+++ bot.py
@@ -5,28 +5,42 @@ from discord.ext import commands
 
 from cogs import MusicCog
 
-intents = discord.Intents.default()
-bot = commands.Bot('-', intents=intents)
+
+class Bot(commands.Bot):
+    def __init__(self, token: str, **kwargs):
+        if not kwargs.get('command_prefix'):
+            kwargs['command_prefix'] = '!'
+        if not kwargs.get('intents'):
+            kwargs['intents'] = discord.Intents.default()
+
+        super().__init__(**kwargs)
+        self.token = token
+
+    async def wrapped_connect(self):
+        await self.add_cog(MusicCog(self))
+        await self.login(self.token)
+        try:
+            await self.connect()
+        except Exception as e:
+            await self.close()
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user.name}')
+        await self.tree.sync()
 
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    await bot.tree.sync()
-    print('Synced commands tree')
-
-
-async def main(token: str):
-    music_cog = MusicCog(bot)
-    await bot.add_cog(music_cog)
-    await bot.start(token)
-
-token = os.environ.get('TOKEN')
-if not token:
-    try:
-        with open('token.txt', 'r') as f:
-            token = f.read()
-    except FileNotFoundError:
-        print('Token not found. Either set the TOKEN environment variable or create a file called "token.txt" with the token in it.')
-        exit(1)
-asyncio.run(main(token))
+if __name__ == '__main__':
+    tokens = os.environ.get('TOKENS') or os.environ.get('TOKEN')
+    if not tokens:
+        try:
+            with open('tokens.txt', 'r') as f:
+                tokens = f.read()
+        except FileNotFoundError:
+            print('Tokens not found. Either set the TOKENS environment variable or create a file called "tokens.txt" with the tokens separated by spaces or newlines.')
+            exit(1)
+    tokens = tokens.split()
+    bots = [Bot(token) for token in tokens]
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(
+        *[bot.wrapped_connect() for bot in bots]))
+    loop.run_forever()
