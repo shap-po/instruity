cdade2accd437882702dcef479d3042f288aea6c
Shap <slavko.ukrop@gmail.com>
2023-04-06 21:10:50 +0300

    another possible fix for connection error added reconnect_on_network_error and reconnect_on_http_error reconnect_delay_max 5 ->10

--- cogs/music.py
+++ cogs/music.py
@@ -39,7 +39,7 @@ YTDL_OPTIONS = {
     'socket_timeout': 10,
 }
 FFMPEG_OPTIONS = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_on_network_error 1 -reconnect_on_http_error 1 -reconnect_delay_max 10',
     'options': '-vn',
 }
 
